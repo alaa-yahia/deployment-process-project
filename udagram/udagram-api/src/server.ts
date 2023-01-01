6d2d2e3a34cbe9fa4d3cbe98b4f32bde414636a3
Alaa Yahia <alaay873@live.com>
2023-01-01 20:20:35 +0200

    Change sequilize

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -25,7 +25,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = 8080;
+  const port = process.env.PORT;
 
   app.use(bodyParser.json());
 
@@ -56,6 +56,5 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   app.listen(port, () => {
     console.log(`Backend server is listening on port ${port}....`);
     console.log(`Frontent server running ${process.env.URL}`);
-    console.log(`press CTRL+C to stop server`);
   });
 })();
