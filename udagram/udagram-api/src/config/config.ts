19b571727ff11d7cd4b5fa45043c8197a16a8279
Alaa Yahia <alaay873@live.com>
2023-01-15 18:06:17 +0200

    rds instance working

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,19 +1,22 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// ENV variables 
+// ENV variables
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
+  dbUsername: `${process.env.POSTGRES_USER}`,
+  dbPassword: process.env.POSTGRES_PASSWORD,
+  dbName: process.env.POSTGRES_DB,
+  dbHost: process.env.POSTGRES_HOST,
+  dbPort: process.env.POSTGRES_PORT,
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
+  aws_access_key: process.env.AWS_ACCESS_KEY_ID,
+  aws_access__secret_key: process.env.AWS_SECRET_ACCESS_KEY,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,6 +1,14 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
+/* export const sequelize = new Sequelize({
+  host: config.dbHost,
+  username: config.dbUsername,
+  password: config.dbPassword,
+  database: config.dbName,
+  dialect: "postgres",
+}); */
+
 export const sequelize = new Sequelize(
-  'postgres://alaa@localhost:5432/deploy'
-  );
+  "postgres://postgres:CkZYE0KU6y23PZaZxmbv@database-1.cskqfl0lhxx2.us-east-1.rds.amazonaws.com:5432/postgres"
+);
